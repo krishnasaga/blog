309d1c9cb96a9b927569829c6f6d6ca8076aed6f
Krishna Sagar R <ragas.sagar.474@gmail.com>
2023-04-29 09:54:28 +0100

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,15 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Sagar`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `Sagar`,
+      summary: `A human born in a costal town in south part of India`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    description: `|`,
     siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `solo474`,
     },
   },
   plugins: [
@@ -110,8 +110,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `Gatsby`,
+        name: `Sagar`,
+        short_name: `Sagar`,
         start_url: `/`,
         background_color: `#ffffff`,
         // This will impact how browsers show your PWA/website
